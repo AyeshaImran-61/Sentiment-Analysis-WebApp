4b819660e0e1f33072c7b55f334bd329367a7f8b
Ayesha Imran <mianayesha.25@gmail.com>
2026-07-14 20:41:06 +0500

    Fix Python imports for Vercel

--- api/main.py
+++ api/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 
-from api.utils import preprocess_text
+from utils import preprocess_text
 
 app = FastAPI(
     title="Sentiment Analysis API",
